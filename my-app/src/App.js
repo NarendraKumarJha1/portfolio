d63045832291bdfd1d00cb33f9b4e97e53f9d1bb
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-02-16 03:01:43 +0530

    completed frontened skeleton

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,11 @@ import About from './Compo/About';
 import Facts from './Compo/Facts';
 import Skills from './Compo/Skills';
 import Resume from './Compo/Resume';
+import Portfolio from './Compo/Portfolio';
+import Services from './Compo/Services';
+import Portfoliodetails from './Compo/Portfoliodetails';
+import Footer from './Compo/Footer';
+
 
 function App() {
   return (
@@ -15,6 +20,10 @@ function App() {
      <Facts/>
      <Skills/>
      <Resume/>
+     <Portfolio/>
+     <Portfoliodetails/>
+     <Services/>
+      <Footer/>
     </div>
   );
 }
