f85fa45e245374d7627c86188b30c62fd68fadfc
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-02-16 01:44:46 +0530

    Implemented Hero and Header section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from '../src/Compo/Header';
+import Hero from './Compo/Hero';
+import About from './Compo/About';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Hero/>
+     <About/>
     </div>
   );
 }
